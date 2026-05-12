46a94445139621ac958127bb54e6358a23f1e77b
alexonplus <oleks9185@gmail.com>
2026-05-12 12:44:25 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+namespace TestTestTest
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTestTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
